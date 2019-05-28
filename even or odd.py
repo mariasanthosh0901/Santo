27cfc236a0a6ac5cff054dcba7012576132e0387
Maria Santhosh Prabhu A <46143923+mariasanthosh0901@users.noreply.github.com>
2019-05-28 07:18:50 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,7 +1,8 @@
 numz=int(input())
-if(numz%2==0):
-       print("Even")
-elif(numz%2!=0):
-       print("Odd")
+if(numz>0):
+       if(numz%2==0):
+              print("Even")
+       elif(numz%2!=0):
+              print("Odd")
 else:
        print("Invalid")
