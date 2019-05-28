f376885526b2a6affd82599171e9d115542c3c44
Maria Santhosh Prabhu A <46143923+mariasanthosh0901@users.noreply.github.com>
2019-05-29 00:04:51 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,11 +1,11 @@
-num = int(input())
+nu = int(input())
 sum = 0
-temp = num
+temp = nu
 while temp > 0:
    digit = temp % 10
    sum += digit ** 3
    temp //= 10
-if num == sum:
+if nu == sum:
    print("yes")
 else:
    print("no")
