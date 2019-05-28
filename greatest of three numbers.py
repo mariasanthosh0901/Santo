e786623293f16f9cb32c4616e47096add97d6609
Maria Santhosh Prabhu A <46143923+mariasanthosh0901@users.noreply.github.com>
2019-05-28 07:59:13 +0530

    Update greatest of three numbers.py

--- greatest of three numbers.py
+++ greatest of three numbers.py
@@ -1,7 +1,7 @@
 z1,z2,z3=map(int,input().split())
-if(z1>z2 and z1>z3):
+if(z1>=z2 and z1>=z3):
        print(z1)
-elif(z2>z1 and z2>z3):
+elif(z2>=z1 and z2>=z3):
        print(z2)
 else:
        print(z3)
