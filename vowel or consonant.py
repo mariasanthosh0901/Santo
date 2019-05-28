94bb486c1f41c35b79baccafe47178e69d2253ae
Maria Santhosh Prabhu A <46143923+mariasanthosh0901@users.noreply.github.com>
2019-05-28 07:44:43 +0530

    Update vowel or consonant.py

--- vowel or consonant.py
+++ vowel or consonant.py
@@ -1,5 +1,5 @@
 strz=input()
-if(ord(strz>=97) and ord(strz<=127)):
+if(ord(strz)>=97 and ord(strz)<=127):
        if(strz=='a' or strz=='e' or strz=='i' or strz=='o' or strz=='u'):
               print("Vowel")
        else:
