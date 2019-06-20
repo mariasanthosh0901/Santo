fa99bb7f2bde69cb3c1c0a640d17f34dec026f73
Maria Santhosh Prabhu A <46143923+mariasanthosh0901@users.noreply.github.com>
2019-06-20 18:00:59 +0530

    Update maximal.py

--- maximal.py
+++ maximal.py
@@ -1,15 +1,29 @@
 n=input()
-s=list(set(n))
-count=0
-for i in n:
-    if i in s:
-        count+=1
-        s.remove(i)
-    else:
-        count+=1
-        continue
-    if(len(s)!=0):
-        continue
-    else:
-        break
-print(count)
+max1=len(n)
+w=len(n)
+x=0
+def val(x1,w,n,s,max1):
+       count=0
+       for i in range(x1,w):
+              if n[i] in s:
+                     count+=1
+                     s.remove(n[i])
+              else:
+                     count+=1
+                     continue
+              if(len(s)!=0):
+                     continue
+              else:
+                     break
+       if(len(s)==0):
+              return count
+       else:
+              return max1
+for i in range(x,w):
+       s=list(set(n))
+       res=val(x,w,n,s,max1)
+       if(res<max1):
+              max1=res
+              res=0
+       x+=1
+print(max1)
